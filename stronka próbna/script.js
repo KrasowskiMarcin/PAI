8ff11eddc18df0615c9fc5da72eb5a652f8e2eb2
KrasowskiMarcin <mkrasowski146@technikumkreatywne.pl>
2022-09-08 10:59:46 +0200

    strona logowania pierwszy commit

---
+++ stronka próbna/script.js
@@ -0,0 +1,9 @@
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const repeatedPassword = document.getElementById("repeatedPassword");
+
+
+document.getElementById("buttonSubmit").addEventListener("click", function(event){
+  event.preventDefault()
+});
