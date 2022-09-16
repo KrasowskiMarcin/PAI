25a9186bd862f3a797075deb41fb61cb872cb634
KrasowskiMarcin <102617904+KrasowskiMarcin@users.noreply.github.com>
2022-09-16 10:05:45 +0200

    Delete redundant directory

--- stronka próbna/script.js
+++
@@ -1,9 +0,0 @@
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const repeatedPassword = document.getElementById("repeatedPassword");
-
-
-document.getElementById("buttonSubmit").addEventListener("click", function(event){
-  event.preventDefault()
-});
